574bf32cbc8f4a80f57d82a84554125af4aa9d54
alexhamme123 <ah2166@bard.edu>
2017-04-19 13:25:07 -0400

    Update parseByDays.py

--- parseByDays.py
+++ parseByDays.py
@@ -5,20 +5,23 @@ Created on Tue Nov 29 15:26:50 2016
 @author: Alex Hamme
 """
 
+
 def main(lst):
+    '''
+    Method to split classes up by day of the week. Returns tuple of five lists, one for each day
+    :param lst: list of classes
+    :return: tuple
+    '''
+
     classes = list(lst)
     
-    Mon = []; Tue = []; Wed = []; Thu = []; Fri = []; Sat = []; Sun = []
+    mon, tue, wed, thu, fri = [], [], [], [], []
     
     for cls in classes:
         days = cls[4]
         
-        for lst, dy in zip([Mon,Tue,Wed,Thu,Fri], ['M','Tu','W','Th','F']):
+        for lst, dy in zip([mon, tue, wed, thu, fri], ['M', 'Tu', 'W', 'Th', 'F']):
             if dy in days:
                 lst.append(cls)
-    return (Mon,Tue,Wed,Thu,Fri)
-    
-#if __name__ == '__main__':
-#    import sys
-#    main(sys.argv[1])
-    
+
+    return mon, tue, wed, thu, fri
