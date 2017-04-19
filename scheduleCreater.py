72dcbcad42770fa15b8208c2fbb13c7b4ba9b197
alexhamme123 <ah2166@bard.edu>
2017-04-19 13:23:19 -0400

    Update scheduleCreater.py

--- scheduleCreater.py
+++ scheduleCreater.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Nov 20 21:03:47 2016
-
 @author: Alex Hamme
 """
 from collections import deque
-import scheduleCells as schedCells
 import tkMessageBox, tkSimpleDialog
 import Tkinter as tk
 import dialogWindow
+import scheduleCells
 import time
 import sys
 import os
@@ -22,93 +21,12 @@ widthNumb = 95
     
 with open('schedule'+str(pathNumb)+'.html', 'w') as f: 
     
-    #f = open('schedule'+str(pathNumb)+'.html','w')
     f.write('''<!DOCTYPE html>
                 <html>
                 <head>
                 <title>ScheduleCreater</title>
                 <meta charset="UTF-8" content="width=device-width, initial-scale=1.0">
-                '''#<link rel="stylesheet" href="styleFile.css">
-                '''
-                <style>
-                
-                
-                   body { 
-
-                        font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
-                        background-image: url('images/concrete_wall.png');
-                        background-repeat: repeat;
-                        background-color: gray;
-                    }
-                    
-                    table {
-                    
-                        width: 10%;        
-                        float: left;
-                        
-                        <!--table-layout: fixed;-->
-                        
-                        overflow:auto;<!--hidden?-->
-                        border: solid 1px silver;
-                        border-collapse:collapse;
-                        background-image: url('images/blizzard.png');
-                        background-repeat: repeat;
-                        background-color: ghostwhite;
-                    }
-                    
-                    th
-                    {
-                        margin: 0;
-                        padding: 5px 10px;
-                        text-align: center;
-                        border-collapse: collapse;
-                        border: 1px solid black;
-                    }
-                    
-                    tr {
-                    padding:0px;
-                    border: 1px solid silver;-->
-                    }
-                    
-                    td
-                    {
-                      
-                      text-align: center;
-                      margin: 0; 
-                      padding: 0px 0px;
-                      border-collapse: collapse;
-                      border: 1px solid silver;
-                    }
-                    .borders {
-                       border: 1px solid silver;
-                       border-collapse:collapse;
-                    }
-                    
-                    .noborders td {
-                       border:0;
-                    }
-                    
-                    .timeCell {
-                       padding: 0px 0px;
-                       border: 1px solid black;
-                       margin: 0;
-                       height:60px; 
-                       
-                    }
-                    
-                    .freeCell {
-                    }
-                    
-                    th.timeHeading {
-                        margin:0;
-                        padding: 5px 10px;
-                        text-align: center;
-                        border-collapse: collapse;
-                        border: 1px solid black;
-                        background-color:white;
-                    }
-                    
-                </style>
+                <link rel="stylesheet" type="text/css" href="stylefile.css">
                 </head>
                 <body>''')
     
@@ -163,54 +81,10 @@ with open('schedule'+str(pathNumb)+'.html', 'w') as f:
             
             
             </table>''')
-    
-    
-    
-    
-    """<table style = "width:10%; border-collapse:collapse"><tr><th id = "timeHeading">Time</th></tr>
-            <tr style = "height:60px; border: 1px solid black">
-                <td class = "timeCell">8:00 am</td></tr>
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">9:00 am</td></tr>
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">10:00 am</td></tr>        
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">11:00 am</td></tr> 
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">12:00 pm</td></tr>    
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">1:00 pm</td></tr>
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">2:00 pm</td></tr>    
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">3:00 pm</td></tr>   
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">4:00 pm</td></tr>    
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">5:00 pm</td></tr>   
-            <tr style = "height:60px; border: 1px solid black">
-                <td style = "border: 1px solid black">6:00 pm</td></tr>    
-            
-            </table>
-            
-            """
-    
-    
-    """
-    for i in range(len(classes)):
-        f.write("<th>")
-        f.write("<td><tl>Course: "+classes[i][0]+"</tl></td>")
-        f.write("<td>Room Number: "+classes[i][1]+"</td>")
-        f.write("<td>Time: "+classes[i][2]+"</td>")
-        f.write("<td>Days: "+classes[i][3]+"</td>")
-        f.write("</th>")
-    """
-    html,timeElapsed = schedCells.main()
-    #print html
-    #f.write("<table>")
+
+    html, time_elapsed = scheduleCells.CellScheduler.main()
+
     f.write(str(html))
-    #f.write("</table>")
     f.write('''</body>
                 </html>''')
-print "Elapsed time: {}s".format(timeElapsed)
-#f.close()
+print "Elapsed time: {}s".format(time_elapsed)
